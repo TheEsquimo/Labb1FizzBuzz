1d5e2261210234b5d96d4fbb9c66aa6a74ca9043
Sunse7 <sunsetluna@gmail.com>
2020-11-23 15:59:36 +0100

    Update Program.cs
    FizzBuzz game

--- Program.cs
+++ Program.cs
@@ -2,10 +2,68 @@
 
 namespace Labb1FizzBuzz
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Program program = new Program();
+            program.FizzBuzzRun();
+        }
+
+        public int UserInput()
+        {
+            int inputNumber = 0;
+            while (inputNumber < 1 || inputNumber >= 300)
+            {
+                Console.WriteLine("Pick a number between 1-299:");
+                string input = Console.ReadLine();
+                try
+                {
+                    inputNumber = int.Parse(input);
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid number");
+                }
+            }
+                
+            return inputNumber;
+        }
+
+        public void FizzBuzzRun()
+        {
+            while (true)
+            {
+                var userInput = UserInput();
+                Console.Clear();
+                for (int i = 1; i <= userInput; i++)
+                {
+                    Console.WriteLine(GetFizzBuzzResult(i));
+                }
+
+                Console.ReadKey();
+            }
+        }
+        
+        public string GetFizzBuzzResult(int number)
+        {
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                return "FizzBuzz";
+            }
+            else if (number % 3 == 0)
+            {
+                return "Fizz";
+            }
+            else if (number % 5 == 0)
+            {
+                return "Buzz";
+            }
+            else if (number == 42)
+            {
+                return "Answer to the Ultimate Question of Life, the Universe, and Everything";
+            }
+            return number.ToString();
         }
     }
 }
